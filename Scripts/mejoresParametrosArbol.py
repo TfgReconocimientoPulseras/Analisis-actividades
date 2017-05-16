aca85e5fd8218598a2215bdba5357bfe44f96fb4
Apidal <alex.pidal@hotmail.com>
2017-05-16 19:25:36 +0200

    Script que saca los mejores parametros de las hojas y las muestras del arbol

---
+++ Scripts/mejoresParametrosArbol.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python
+
+import numpy as np
+import pandas as pd
+from numpy import genfromtxt
+from sklearn.tree import DecisionTreeClassifier
+
+
+
+X_train= genfromtxt("X_train_movil2.csv", delimiter=',')
+y_train= genfromtxt("y_train_movil2.csv", delimiter='')
+X_train[np.isnan(X_train)] = np.median(X_train[~np.isnan(X_train)])
+X_val= genfromtxt("X_val_movil2.csv", delimiter=',')
+y_val= genfromtxt("y_val_movil2.csv", delimiter='')
+X_val[np.isnan(X_val)] = np.median(X_val[~np.isnan(X_val)])
+
+
+x = min_samples_split = [0.001, 0.01, 0.025, 0.1, 0.2, 0.5, 50, 25, 10]
+split_len = len(min_samples_split)
+y = min_samples_leaf = [0.001, 0.01, 0.025, 0.1, 0.2, 0.5, 50, 25, 10]
+leaf_len = len(min_samples_leaf)
+
+precision = 0
+split = 0
+leaf = 0
+max_por = 0
+
+for i in range(split_len):
+    for j in range(leaf_len):
+        clf = DecisionTreeClassifier(criterion='entropy',random_state=0, min_samples_split = min_samples_split[i], min_samples_leaf = min_samples_leaf[j])
+        clf = clf.fit(X_train, y_train)
+        precision = clf.score(X_val ,y_val)*100
+        if precision > max_por:
+            max_por = precision
+            split = min_samples_split[i]
+            leaf = min_samples_leaf[j]
+
+print("\n\nMáxima precisión(Accuracy): {},\nmin_samples_split: {}, min_samples_leaf: {}".format(max_por, split, leaf))
